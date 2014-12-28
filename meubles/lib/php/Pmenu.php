51c085d4a4d4a6aed030ed8b113b03d2bbd96f0b
bilgenm <bilgen.mustafa@condorcet.be>
2014-12-28 22:41:01 +0100

    ajout Pmenu

--- meubles/lib/php/Pmenu.php
+++ meubles/lib/php/Pmenu.php
@@ -1,8 +1,7 @@
 <ul id="menu">
- <li class="bottomLine"><a href="index.php?page=accueil">Accueil</a></li>
- <li class="bottomLine"> <a href="#">Catalogue</a></li>
- <li class="bottomLine"> <a href="#">Magasins</a></li>
- <li class="bottomLine"> <a href="#">Panier</a></li>
- <li class="bottomLine"> <a href="#">Se connecter</a></li>
+<li class="bottomLine"><a href="index.php?page=accueil">Accueil</a></li>
+<li class="bottomLine"> <a href="#">Catalogue</a></li>
+<li class="bottomLine"> <a href="#">Magasins</a></li>
+<li class="bottomLine"> <a href="#">Panier</a></li>
+<li class="bottomLine"> <a href="#">Se connecter</a></li>
 </ul>
-
