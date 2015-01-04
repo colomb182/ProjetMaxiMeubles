6b8490e64ab79276aa708d752265d75490dc1501
colomb1982 <lauramagalypaz@hotmail.com>
2015-01-04 04:51:13 +0100

    commit ajout menu_footer et m a j stylepc

---
+++ meubles/lib/php/menu_footer.php
@@ -0,0 +1,14 @@
+
+<div>
+<ul>
+         <li><a href="index.php?page=accueil">Accueil</a></li>
+	 <li> <a href="index.php?page=catalogue">Catalogue</a></li>
+	 <li> <a href="#">Magasins</a></li>
+         <li> <a href="#">Panier</a></li>
+        
+         <li><a href= "../admin/index.php?page=connexionadmin" id="bordure_droite">Administrateur</a></li>
+</ul>
+</div>
+<p> Editeur responsable sitesuperbeaux@condorcet.be</p>
+  
+
